d599f6ff0bfb0566568a2d85317bf5eb50589589
Vivit Rana <vivitrana@Vivits-MacBook-Air.local>
2025-04-06 01:58:55 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import'./App.css'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Login from './Pages/Login'
+import Navbar from './Component/Navbar'
+import Logout from './Pages/Logout'
+
+function App() {
+  return (
+   <>
+      <Navbar/>
+      <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[10vw]">
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/logout' element={<Logout/>}/>
+      </Routes>
+      </div>
+      </>
+  )
+}
+
+export default App
