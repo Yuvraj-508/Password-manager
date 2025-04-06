5e89b5b6571736f1b68ee808d4a0297d128a1b8d
Vivit Rana <vivitrana@Vivits-MacBook-Air.local>
2025-04-06 21:20:00 +0530

    minor

--- src/Component/Form.jsx
+++ src/Component/Form.jsx
@@ -1,5 +1,9 @@
 import React,{useState} from 'react'
+import { Link } from 'react-router';
 import './Form.css'
+import goggle from "../assets/google.png"
+import facebook from "../assets/facebook.png"
+
 function Form() {
     const [isActive, setIsActive] = useState(false);
   return (
@@ -12,23 +16,20 @@ function Form() {
             <div class="input-box">
                 <input type="text" placeholder="Username" 
                 required/>
-                <i class='bx bxs-user'></i>
             </div>
             <div class="input-box">
                 <input type="password" placeholder="password" 
                 required/>
-                <i class='bx bxs-lock-alt'></i>
             </div>
             <div class="forgot-link">
                 <a href="#">Forgot password?</a>
             </div>
             <button type="submit" class="btn">login</button>
             <p>or login with social platforms</p>
-            <div class="social-icons">
-                <a href="#"><i class='bx bxl-google' ></i></a>
-                <a href="#"><i class='bx bxl-facebook' ></i></a>
-                <a href="#"><i class='bx bxl-github' ></i></a>
-                <a href="#"><i class='bx bxl-linkedin' ></i></a>
+            <div class="social-icons flex ">
+             <Link className='w-20 '><img src={goggle} alt="" className='w-15 py-2 ' /></Link>
+             <Link className='w-20 '><img src={facebook} alt="" className='w-15 py-2 ' /></Link>
+
             </div>
         </form>
       </div>
